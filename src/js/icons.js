2c3ab7e029568b9cd4674bc214111cb883ccf227
Crix <okcrix@gmail.com>
2022-07-05 02:14:52 -0500

    add files

---
+++ src/js/icons.js
@@ -0,0 +1,12 @@
+// icons
+
+// Lucide icons
+// https://github.com/lucide-icons/lucide
+// https://iconer.app/lucide/
+
+import { createIcons, CloudLightning, CloudHail, Sun } from 'lucide';
+
+createIcons({
+  attrs: { class: ['icon'] },
+  icons: { CloudLightning, CloudHail, Sun },
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,29 @@
+// animations
+// GSAP https://greensock.com/docs/
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { Observer } from 'gsap/Observer';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { Draggable } from 'gsap/Draggable';
+gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin, Draggable);
+
+// smooth transitions
+// BarbaJS https://barba.js.org/
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
+barba.use(barbaCss);
+
+// smooth scroll
+// Locomotive Scroll https://github.com/locomotivemtl/locomotive-scroll
+import LocomotiveScroll from 'locomotive-scroll';
+const scroll = new LocomotiveScroll();
+
+// modules JS
+// icons
+import './icons.js';
+
+// Styles
 import '../styles/main.scss';
+
+//
 console.log('conectado!');
